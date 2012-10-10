4f28a7051bf2f80bfd74d6582715b47959462001
artw <arthur.wiedmer@berkeley.edu>
2012-10-09 22:24:26 -0700

    added lattice plot of Haruko's TCCZ kriging

---
+++ TCCZ_krig/plotTCCZk.R
@@ -0,0 +1,17 @@
+##Plot Haruko's data
+require(ggplot2)
+require(lattice)
+require(geoR)
+rm(list=ls())
+TCCZkH.tmp<-read.table("TCCZ_krigged.txt",sep="\t", header=TRUE)
+TCCZkH<-TCCZkH.tmp[order(TCCZkH.tmp$X, TCCZkH.tmp$Y),]
+head(TCCZkH)
+# persp(TCCZkH$X, TCCZkH$Y, TCCZkH$Ztccz, phi = 45, theta = 45,
+#       xlab = "UTM E (m)", ylab = "UTM N (m)",
+#       main = "TCCZ top elevation data"
+# )
+wireframe(Ztccz ~ X * Y, data=TCCZkH)
+
+#TCCZ.loess <- loess(Ztccz ~ X*Y, data = TCCZkH, degree = 2, span = 0.25)
+
+#image(TCCZkH$X, TCCZkH$Y, TCCZkH$Ztccz, xlab = "UTM E (m)", ylab = "UTM N (m)",main = "TCCZ top elevation data")
