02ad6e46a4297ef70f3c0c57200713a7c1c918db
artw <arthur.wiedmer@berkeley.edu>
2012-11-02 08:41:11 -0700

    inventory advances

--- inventory/simple_inventory.R
+++ inventory/simple_inventory.R
@@ -7,6 +7,7 @@ TCCZe<-readRDS("TCCZ_o.rdata")
 #
 #summary(tritium[tritium$STATION_ID=='FSB115D',])
 TCCZ.lm<-lm(TCCZ_top~UTM_E+UTM_N,data=TCCZe,na.action=na.omit)
+TCCZ.loess = loess(TCCZ_top~UTM_E+UTM_N, data = TCCZe, degree = 2, span = 0.25)
 plot(TCCZ.lm)
 
 #Define n zones
