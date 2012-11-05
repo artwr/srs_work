4c53d7486549af01b3ae7e37e6a84044ba6c7798
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2012-11-04 21:16:21 -0800

    sync R packages script and list

---
+++ syncRpkgs.R
@@ -0,0 +1,10 @@
+#Sync R packages
+IP <- as.data.frame(installed.packages())
+
+MyPkgs <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
+
+save(MyPkgs, file = "MyPkgs.Rdata")
+
+load("MyPkgs.Rdata")
+
+install.packages(MyPkgs.sl27$Package, dependencies = TRUE)
