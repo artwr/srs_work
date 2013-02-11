9cbfc46905a4688c52ab5f51a134c86bc280f86f
artw <arthur.wiedmer@berkeley.edu>
2013-02-11 09:14:57 -0800

    added correlation loader

---
+++ SRS_data/import_corr.R
@@ -0,0 +1,27 @@
+require(plyr)
+#require(reshape2)
+
+rm(list=ls())
+#read the data
+corr.raw<-read.csv("corr4R.csv")
+
+
+
+#c("STATION_SEQ","STATION_ID","EASTING","NORTHING","LATITUDE","LONGITUDE","GROUND_ELEVATION","REFERENCE_ELEVATION","TOP_ELEV","BOTTOM_ELEV","MDATE","MYEAR","MMONTH","MDAY","MQUARTER")
+
+#MQUARTER = floor(MMONTH/3+2/3)
+#Aggregate by year+quarters
+
+#Aggregate by year
+
+
+
+
+
+
+
+
+
+
+
+
