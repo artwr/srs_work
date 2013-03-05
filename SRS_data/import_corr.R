a15cbb93faad4c7ce6a59a58173eafd2fe111d0c
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2013-03-04 18:10:45 -0800

    added view statement

--- SRS_data/import_corr.R
+++ SRS_data/import_corr.R
@@ -5,7 +5,7 @@ rm(list=ls())
 #read the data
 corr.raw<-read.csv("corr4R.csv")
 
-
+head(corr.raw)
 
 #c("STATION_SEQ","STATION_ID","EASTING","NORTHING","LATITUDE","LONGITUDE","GROUND_ELEVATION","REFERENCE_ELEVATION","TOP_ELEV","BOTTOM_ELEV","MDATE","MYEAR","MMONTH","MDAY","MQUARTER")
 
