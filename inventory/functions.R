717626a005150c700ff6c97bd33ba1bad2e4c5c8
artw <arthur.wiedmer@berkeley.edu>
2013-04-14 22:52:17 -0700

    added function to compute decay corrected cumulative sum. Works for inputs as well as outputs.

--- inventory/functions.R
+++ inventory/functions.R
@@ -1,6 +1,25 @@
 require(plyr)
 
+
+# Function to compute the minimum bounding rectangle 
+# for each year in the dataset
 minboundrect<-function(interpdataset) {ddply(interpdataset, c('MYEAR'), function(x) c(datacount=nrow(x),x.min=min(x$EASTING),x.max=max(x$EASTING),y.min=min(x$NORTHING),y.max=max(x$NORTHING)))}
+
+
+# Function to compute the cumulative input decay corrected
+# Using a vector containing the original inputs
+# and a constant containing the half-life in years
+
+cumsumdecaycorrected<-function(yearlyvalues,halflife) {
+  k<- log(2)/halflife; ek<-exp(-k);
+  n<-length(yearlyvalues);v<-ek^(0:n-1);
+  MM<-rep.int(0,n) %o% rep.int(0,n);
+  for (jj in 1:(n-1)) {
+    MM[jj:n,jj]<-v[1:(n-(jj-1))]
+  }
+  return (MM %*% yearlyvalues)
+}
+
 
 #basin coords for plotting if needed
 #f3basin<-readRDS("../basin_coords/f3basin.rdata")
