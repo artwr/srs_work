53641badd7577c3620c33428864ece3d64576bca
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2013-05-19 14:26:56 -0700

    added folder in path for FAW5 script and graph

--- FAW5/import_and_clean_nitrate.R
+++ FAW5/import_and_clean_nitrate.R
@@ -1,5 +1,5 @@
 #
 require(ggplot2)
-nitrateFAW5<-read.csv("nitrate_FAW5.csv",header = TRUE)
+nitrateFAW5<-read.csv("../FAW5/nitrate_FAW5.csv",header = TRUE)
 names(nitrateFAW5)<-c("nitratemgl","delta.elev","depth.from.wt")
-saveRDS(nitrateFAW5, file = "nitrateFAW5.rdata")
+saveRDS(nitrateFAW5, file = "../FAW5/nitrateFAW5.rdata")
