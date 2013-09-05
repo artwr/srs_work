fd5fd00bb893fa48a62c6074c79a74126ff86341
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2013-09-05 16:56:24 -0700

    sync packages.

--- syncRpkgs.R
+++ syncRpkgs.R
@@ -3,8 +3,9 @@ IP <- as.data.frame(installed.packages())
 
 MyPkgs <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
 
-save(MyPkgs, file = "MyPkgs.Rdata")
+# save(MyPkgs, file = "MyPkgs_laptop.Rdata")
+# save(MyPkgs, file = "MyPkgs_desktop.Rdata")
 
-load("MyPkgs.Rdata")
+# load("MyPkgs.Rdata")
 
-install.packages(MyPkgs$Package, dependencies = TRUE)
+# install.packages(MyPkgs$Package, dependencies = TRUE)
