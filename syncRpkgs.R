cf058dede86c576594fb9a58a3962be96361d0ec
artw <arthur.wiedmer@berkeley.edu>
2013-09-05 17:53:18 -0700

    synced packages desktop.

--- syncRpkgs.R
+++ syncRpkgs.R
@@ -2,18 +2,15 @@
 IP <- as.data.frame(installed.packages())
 
 # MyPkgs_laptop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
-# save(MyPkgs, file = "MyPkgs_laptop.Rdata")
+# save(MyPkgs_laptop, file = "MyPkgs_laptop.Rdata")
 # load("MyPkgs_desktop.Rdata")
 # ptoinstall<-setdiff(MyPkgs_desktop$Package,MyPkgs_laptop$Package)
 
 
 # MyPkgs_desktop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
-# save(MyPkgs, file = "MyPkgs_desktop.Rdata")
+# save(MyPkgs_desktop, file = "MyPkgs_desktop.Rdata")
 # load("MyPkgs_laptop.Rdata")
 # ptoinstall<-setdiff(MyPkgs_laptop$Package,MyPkgs_desktop$Package)
 
-
-
-
-# install.packages(MyPkgs$Package, dependencies = TRUE)
+#Install the packages that need to be.
 install.packages(ptoinstall, dependencies = TRUE)
