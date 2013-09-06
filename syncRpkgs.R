f623fa96021cb3dab4af570210e1a1b5b2cdd09d
artw <arthur.wiedmer@berkeley.edu>
2013-09-05 17:41:53 -0700

    simplified syncing of packages.

--- syncRpkgs.R
+++ syncRpkgs.R
@@ -1,11 +1,19 @@
 #Sync R packages
 IP <- as.data.frame(installed.packages())
 
-MyPkgs <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
-
+# MyPkgs_laptop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
 # save(MyPkgs, file = "MyPkgs_laptop.Rdata")
-# save(MyPkgs, file = "MyPkgs_desktop.Rdata")
+# load("MyPkgs_desktop.Rdata")
+# ptoinstall<-setdiff(MyPkgs_desktop$Package,MyPkgs_laptop$Package)
 
-# load("MyPkgs.Rdata")
+
+# MyPkgs_desktop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
+# save(MyPkgs, file = "MyPkgs_desktop.Rdata")
+# load("MyPkgs_laptop.Rdata")
+# ptoinstall<-setdiff(MyPkgs_laptop$Package,MyPkgs_desktop$Package)
+
+
+
 
 # install.packages(MyPkgs$Package, dependencies = TRUE)
+install.packages(ptoinstall, dependencies = TRUE)
