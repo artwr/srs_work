b2f8d5f3c6c94bab488a4fffe7149a4dc828a6a7
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2013-10-22 23:16:28 -0700

    slight mods to package sync script.

--- syncRpkgs.R
+++ syncRpkgs.R
@@ -5,7 +5,7 @@ IP <- as.data.frame(installed.packages())
 # save(MyPkgs_laptop, file = "MyPkgs_laptop.Rdata")
 # load("MyPkgs_desktop.Rdata")
 # ptoinstall<-setdiff(MyPkgs_desktop$Package,MyPkgs_laptop$Package)
-
+ptoinstall<-MyPkgs_laptop$Package
 
 # MyPkgs_desktop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
 # save(MyPkgs_desktop, file = "MyPkgs_desktop.Rdata")
@@ -13,4 +13,6 @@ IP <- as.data.frame(installed.packages())
 # ptoinstall<-setdiff(MyPkgs_laptop$Package,MyPkgs_desktop$Package)
 
 #Install the packages that need to be.
-install.packages(ptoinstall, dependencies = TRUE)
+# install.packages(as.character(ptoinstall), dependencies = TRUE, type="both")
+# install.packages(as.character(ptoinstall), type="both")
+# install.packages(as.character(ptoinstall))
