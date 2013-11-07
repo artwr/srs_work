fef5917ca9c50b96aef76daf900f7973436f982f
artw <arthur.wiedmer@berkeley.edu>
2013-11-07 11:31:54 -0800

    minor changes to SyncRpkgs.R

--- syncRpkgs.R
+++ syncRpkgs.R
@@ -5,12 +5,13 @@ IP <- as.data.frame(installed.packages())
 # save(MyPkgs_laptop, file = "MyPkgs_laptop.Rdata")
 # load("MyPkgs_desktop.Rdata")
 # ptoinstall<-setdiff(MyPkgs_desktop$Package,MyPkgs_laptop$Package)
-ptoinstall<-MyPkgs_laptop$Package
+# ptoinstall<-MyPkgs_laptop$Package
 
 # MyPkgs_desktop <- subset(IP, !Priority %in% c("base", "recommended"), select =  c(Package))
 # save(MyPkgs_desktop, file = "MyPkgs_desktop.Rdata")
 # load("MyPkgs_laptop.Rdata")
 # ptoinstall<-setdiff(MyPkgs_laptop$Package,MyPkgs_desktop$Package)
+# ptoinstall<-setdiff(MyPkgs_desktop$Package,MyPkgs_desktop2$Package) 
 
 #Install the packages that need to be.
 # install.packages(as.character(ptoinstall), dependencies = TRUE, type="both")
