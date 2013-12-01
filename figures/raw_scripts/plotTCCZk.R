223e391df417d8c249683ca3bb767ae2c26665d4
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2013-12-01 04:34:51 -0800

    renamed script for consistency and changed paths

--- figures/raw_scripts/plotTCCZk.R
+++ figures/raw_scripts/plotTCCZk.R
@@ -3,7 +3,7 @@ require(ggplot2)
 require(lattice)
 require(geoR)
 rm(list=ls())
-TCCZkH.tmp<-read.table("TCCZ_krigged.txt",sep="\t", header=TRUE)
+TCCZkH.tmp<-read.table("../../geo_data/raw/TCCZ_krigged.txt",sep="\t", header=TRUE)
 TCCZkH<-TCCZkH.tmp[order(TCCZkH.tmp$X, TCCZkH.tmp$Y),]
 head(TCCZkH)
 # persp(TCCZkH$X, TCCZkH$Y, TCCZkH$Ztccz, phi = 45, theta = 45,
