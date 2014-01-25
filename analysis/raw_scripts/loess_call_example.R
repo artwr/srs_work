c929495ba96f1f26583c85607f5f92993aa21e8b
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-01-24 18:22:38 -0800

    added skeleton example of a loess call for reuse in a function

---
+++ analysis/raw_scripts/loess_call_example.R
@@ -0,0 +1,20 @@
+#loess call
+
+loess.control(surface = c("interpolate", "direct"),
+              statistics = c("approximate", "exact"),
+              trace.hat = c("exact", "approximate"),
+              cell = 0.2, iterations = 4, ...)
+
+lcontrol<-loess.control(surface = c("direct"),
+              statistics = c("exact"),
+              trace.hat = c("exact"),
+              cell = 0.2, iterations = 5)
+
+
+
+loess(formula, data, weights, subset, na.action, model = FALSE,
+      span = 0.25, degree = 2,
+      normalize = FALSE,
+      family = c("gaussian", "symmetric"),
+      method = c("loess"),
+      control = lcontrol)
