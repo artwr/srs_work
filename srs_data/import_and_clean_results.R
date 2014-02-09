6b9721e9585e77f82f817e47390e024bb6977389
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-02-09 01:47:53 -0800

    created script for import of the raw results table. Ran to create the rdata objects.

---
+++ srs_data/import_and_clean_results.R
@@ -0,0 +1,51 @@
+# import_and_clean_results
+
+require(data.table)
+
+# results.raw <- read.csv("./srs_data/raw/results4R.csv",sep="|", strip.white = TRUE, nrows = 100)
+# resultsC.raw <- read.csv("./srs_data/raw/resultsC4R.csv",sep="|", strip.white = TRUE, nrows = 100)
+
+# names(results.raw)
+# "STATION_SEQ"         "STATION_ID"          "EASTING"             "NORTHING"           
+# [5] "LATITUDE"            "LONGITUDE"           "ANALYSIS_DATE"       "COLLECTION_DATE"    
+# [9] "MDATE"               "MYEAR"               "MMONTH"              "MDAY"               
+# [13] "MQUARTER"            "ANALYTE_NAME"        "RESULT"              "RESULT_UNITS"       
+# [17] "PQL"                 "PQL_UNITS"           "RESULT_SEQ"          "SAMPLE_SEQ"         
+# [21] "GROUND_ELEVATION"    "REFERENCE_ELEVATION" "TOP_ELEV"            "BOTTOM_ELEV" 
+
+resultsrawClasses <- c("factor", "factor", "numeric", "numeric", "numeric", "numeric", "character", "character", "Date", "integer", "integer", "integer", "integer", "factor", "numeric", "character", "numeric", "character", "factor", "factor", "numeric", "numeric", "numeric", "numeric")
+
+results.raw <- read.csv("./srs_data/raw/results4R.csv",sep="|", strip.white = TRUE, colClasses = resultsrawClasses)
+resultsC.raw <- read.csv("./srs_data/raw/resultsC4R.csv",sep="|", strip.white = TRUE, colClasses = resultsrawClasses)
+
+
+# results.raw$PQL_UNITS[results.raw$PQL_UNITS == ""] 
+# Testing that the PQL_UNITS are the same as the RESULT_UNITS, except when PQL = NA
+# all.equal(results.raw$RESULT_UNITS, results.raw$PQL_UNITS)
+# identical(results.raw$RESULT_UNITS, results.raw$PQL_UNITS)
+# head(results.raw[results.raw$RESULT_UNITS != results.raw$PQL_UNITS, ])
+# head(results.raw[results.raw$PQL_UNITS == "" & !is.na(results.raw$PQL),])
+
+
+# Remove MDATE
+# results <- results.raw[, c(1:8, 14:24)]
+
+resultsDT <- as.data.table(results.raw)
+resultsCDT <- as.data.table(resultsC.raw)
+
+# Make the conversion to Date objects
+resultsDT$CDATE <- as.Date(resultsDT$COLLECTION_DATE)
+resultsDT$ADATE <- as.Date(resultsDT$ANALYSIS_DATE)
+resultsCDT$CDATE <- as.Date(resultsCDT$COLLECTION_DATE)
+resultsCDT$ADATE <- as.Date(resultsCDT$ANALYSIS_DATE)
+
+# Create ZDATE which will be the reference date for comparison between datapoints
+resultsDT$ZDATE <- resultsDT$CDATE
+resultsDT$ZDATE[is.na(resultsDT$ZDATE)] <- resultsDT$ADATE[is.na(resultsDT$ZDATE)]
+resultsCDT$ZDATE <- resultsCDT$CDATE
+resultsCDT$ZDATE[is.na(resultsDT$ZDATE)] <- resultsCDT$ADATE[is.na(resultsDT$ZDATE)]
+
+saveRDS(as.data.frame(resultsDT), "./srs_data/processed/results.rdata")
+saveRDS(as.data.frame(resultsCDT), "./srs_data/processed/resultsC.rdata")
+
+rm(list = c("results.raw", "resultsC.raw", "resultsDT", "resultsCDT"))
