771c49b0383c5784a8ad2cc3828708e5e09fe984
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-02-17 16:34:42 -0800

    added alphavectorloess for call to loess with multiple alphas

--- analysis/raw_scripts/functions_computation.R
+++ analysis/raw_scripts/functions_computation.R
@@ -14,3 +14,15 @@ require(plyr)
 aggregateAnalyteByYear <- function(analyte.df){
   analyte.aggreg<-ddply(analyte.df, c('STATION_SEQ','STATION_ID','ZYEAR','EASTING','NORTHING'), function(x) c(count=nrow(x),mean=mean(x$RESULT),median=median(x$RESULT),sd=sd(x$RESULT),mad=mad(x$RESULT),min=min(x$RESULT),max=max(x$RESULT)))
 }
+
+# function to compute loess models with a range of alphas
+# It uses the parameters I need in most of my cases
+
+loessAlphaVect <- function(lformula, ldata, ldegree, spanvector = c(.25,.5,.75), lcontrol) {
+  loess.models <-list() 
+  for (k in 1:length(spanvector)) { 
+    loess.models[paste0("alpha",spanvector[k])] <- list(loess(as.formula(lformula), data = ldata, degree = ldegree, span = spanvector[k], normalize = FALSE, method = c("loess"), control = lcontrol))
+    #     loess.models <- c(loess.models, list(loess(as.formula(lformula), data = ldata, degree = ldegree, span = spanvector[k], normalize = FALSE, method = c("loess"), control = lcontrol)))
+  }
+  return(loess.models)
+}
