e913c2ad6917b88f60a60d45182c9bbe4b237552
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-02-17 16:36:32 -0800

    added d3 wells to wl import script and saved rdata files.

--- srs_data/import_wl.R
+++ srs_data/import_wl.R
@@ -2,17 +2,21 @@
 #require(doBy)
 require(plyr)
 
+
 rm(list=ls())
 #read the data
 # wl.raw<-read.table("./srs_data/raw/wl4R.csv",sep="|",header = TRUE, nrows = 15)
 wl.raw<-read.table("./srs_data/raw/wl4R.csv",sep="|",header = TRUE)
-
+wld3.raw<-read.table("./srs_data/raw/wld34R.csv",sep="|",header = TRUE)
 #Remove FEX8 because of the 2 screens.
 wl.clean<-wl.raw[!wl.raw$STATION_ID=='FEX  8' & !wl.raw$STATION_ID=='FEX  9',]
+wld3.clean<-wld3.raw[!wld3.raw$STATION_ID=='FEX  8' & !wld3.raw$STATION_ID=='FEX  9',]
 #wl.clean<-wl.raw[!wl.raw$STATION_ID=='FEX  9',]
-
 #Visual inspection of the data
 #wl.fex<-wl.raw[wl.raw$STATION_ID=='FEX  8' | wl.raw$STATION_ID=='FEX  9',]
+
+saveRDS(wl.clean, file = "./srs_data/processed/wlclean.rdata")
+saveRDS(wld3.clean, file = "./srs_data/processed/wld3clean.rdata")
 
 #For the water levels
 wl.plyr1<-ddply(wl.clean, c('STATION_SEQ','STATION_ID','MYEAR','EASTING','NORTHING'), function(x) c(count=nrow(x),mean=mean(x$WATER_ELEV),median=median(x$WATER_ELEV),sd=sd(x$WATER_ELEV),mad=mad(x$WATER_ELEV),min=min(x$WATER_ELEV),max=max(x$WATER_ELEV)))
@@ -20,7 +24,7 @@ wl.plyr1<-ddply(wl.clean, c('STATION_SEQ','STATION_ID','MYEAR','EASTING','NORTHI
 #average across years
 wl.plyra<-ddply(wl.clean, c('STATION_SEQ','STATION_ID','EASTING','NORTHING'), function(x) c(count=nrow(x),mean=mean(x$WATER_ELEV),median=median(x$WATER_ELEV),sd=sd(x$WATER_ELEV),mad=mad(x$WATER_ELEV),min=min(x$WATER_ELEV),max=max(x$WATER_ELEV)))
 
-saveRDS(wl.plyr1, file = "./srs_data/processed/wl.rdata")
-saveRDS(wl.plyra, file = "./srs_data/processed/wlavg.rdata")
+saveRDS(wl.plyr1, file = "./srs_data/processed/wlavgpwpy.rdata")
+saveRDS(wl.plyra, file = "./srs_data/processed/wlavgpw.rdata")
 
 rm(list = ls())
