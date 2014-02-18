d4fdabdc3af6dc2caa4c3e377ea4b51fa9735882
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-02-17 16:25:57 -0800

    merged the two lists of picks for the TCCZ. Created subset data file required for analysis.

--- geo_data/import_and_clean_TCCZdata.R
+++ geo_data/import_and_clean_TCCZdata.R
@@ -1,17 +1,18 @@
-TCCZdata<-read.csv("raw/TCCZ_nearF.csv")
-TCCZdatac<-TCCZdata
+#TCCZdata<-read.csv("./geo_data/raw/TCCZ_nearF.csv")
+TCCZdata<-read.csv("./geo_data/processed/TCCZ_definitive_all.csv")
 ##Convert non meaningful zeros to NAs
-TCCZdatac[TCCZdatac==0]<-NA
-
+TCCZdata[TCCZdata==0]<-NA
 ##Save as flat object
-saveRDS(TCCZdatac, file = "processed/TCCZ_0toNA.rdata")
-
-colnames(TCCZdatac)[1:3]<-c("STATION_ID","EASTING","NORTHING")
-colnames(TCCZdatac)
+saveRDS(TCCZdata, file = "./geo_data/processed/TCCZ_definitive_all.rdata")
 
 # Remove the points with no TCCZ pick. 
-TCCZdatac2<-TCCZdatac[!is.na(TCCZdatac$TCCZ_top),]
+TCCZdata2<-TCCZdatac[!is.na(TCCZdatac$TCCZ_top),]
 
 ##Save to compressed object
-saveRDS(TCCZdatac, file = "processed/TCCZ_all.rdata")
-saveRDS(TCCZdatac2, file = "processed/TCCZ_wtoppick.rdata")
+# saveRDS(TCCZdata, file = "./geo_data/processed/TCCZ_all.rdata")
+saveRDS(TCCZdata, file = "./geo_data/processed/TCCZ_definitive_all.rdata")
+saveRDS(TCCZdata2, file = "./geo_data/processed/TCCZ_wtoppick.rdata")
+
+TCCZ_nearF <- TCCZdata2[TCCZdata2$EASTING > 435000 & TCCZdata2$EASTING < 438000 & TCCZdata2$NORTHING < 3682700 & TCCZdata2$NORTHING > 3680500, ]
+
+saveRDS(TCCZ_nearF, "./geo_data/processed/TCCZ_nearF_final.rdata")
--- geo_data/import_and_clean_picks_exceldata.R
+++ geo_data/import_and_clean_picks_exceldata.R
@@ -2,7 +2,7 @@
 #
 require(data.table)
 picks_raw <- read.table( "./geo_data/raw/hydropick.txt" ,sep = "\t", header = TRUE)
-
+dim(picks_raw)
 source('./geo_data/srs2utm.R')
 
 utm1 <- c(437504.53, 3681779.12)
@@ -19,12 +19,25 @@ Mtransform <- findTransformationMatrix(srsc, utmc)
 
 picksutm27coords <- srs2utm27(as.matrix(picks_raw[,c("SRSEasting", "SRSNorthing")]), Mtransform)
 picks_all <- cbind(picks_raw, picksutm27coords)
+# dim(picks_all)
 # names(picks_all)
-names(picks_all)[15:16] <- c("EASTING","NORTHING")
-
+names(picks_all)[c(1,15,16)] <- c("STATION_ID","EASTING","NORTHING")
+picks_all$STATION_ID <- sub("-","",picks_all$STATION_ID)
+picks_all$TCCZ_bot <- picks_all$LAZ_top
+picks_all$TCCZ_th_ft <- picks_all$TCCZ_top - picks_all$TCCZ_bot
+saveRDS(picks_all,"./geo_data/processed/picks_all_from_excel.rdata")
+write.csv(picks_all[,c("STATION_ID","EASTING","NORTHING","SurfaceElevation","TCCZ_top","TCCZ_bot","TCCZ_th_ft")],"./geo_data/processed/picks_all_from_excel_file.csv")
+picks_all_nearF <- subset(picks_all, EASTING > 435000 & EASTING < 438000 & NORTHING < 3682700 & NORTHING > 3680500)
 # picks_all[picks_all$WellID == "HSB-143C",c("EASTING","NORTHING")]
 # picks_all[picks_all$WellID == "FSB-98A",c("EASTING","NORTHING")]
 # picks_all[picks_all$WellID == "FSB-79A",c("EASTING","NORTHING")]
 
+write.csv(picks_all[c("STATION_ID","EASTING","NORTHING",)],"./geo_data/processed/picks_all_from_excel_file.csv")
 saveRDS(picks_all,"./geo_data/processed/picks_all_from_excel.rdata")
-saveRDS(picks_all[,c("WellID","EASTING","NORTHING", "TCCZ_top")], "./geo_data/processed/TCCZ_all_from_excel.rdata")
+saveRDS(picks_all_nearF,"./geo_data/processed/picks_all_nearF_from_excel.rdata")
+#
+names(picks_all_nearF)
+# picks_all<-readRDS("./geo_data/processed/picks_all_from_excel.rdata")
+picks_all_nearF$TCCZ_bot <- picks_all_nearF$LAZ_top
+picks_all_nearF$TCCZ_th_ft <- picks_all_nearF$TCCZ_top - picks_all_nearF$TCCZ_bot 
+saveRDS(picks_all_nearF[,c("STATION_ID","EASTING","NORTHING","SurfaceElevation","TCCZ_top","TCCZ_bot","TCCZ_th_ft")], "./geo_data/processed/TCCZ_all_nearF_from_excel.rdata")
